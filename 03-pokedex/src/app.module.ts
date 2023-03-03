7462e1b63776d662316322c84006d0264801543d
wagnercastillo <wagner.castillo@unl.edu.ec>
2023-03-03 18:32:03 -0500

    App Pokedex - Integration with Resource CLI-Docker

---
+++ 03-pokedex/src/app.module.ts
@@ -0,0 +1,18 @@
+import { join } from 'path'; // Node
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+    PokemonModule
+  ],
+  
+})
+
+
+export class AppModule {}

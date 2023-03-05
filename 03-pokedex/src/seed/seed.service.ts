3a91e89e5ed4095575db152526d28898935537f8
WagnerCastillo <wagner.castillo@unl.edu.ec>
2023-03-05 17:26:45 -0500

    Integration with Axios and obtencion of values

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interace';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return `ExecutedSeed`
+  private readonly axios: AxiosInstance = axios;
+
+  
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
+
+    data.results.forEach( ({ name, url })=> {
+
+      const segments = url.split('/');
+      const no:number = +segments[ segments.length - 2 ]
+      
+      console.log({name, no})
+      
+    } )
+    return  data.results  ;
   }
 
+  
 }

076bd399379582e335ed6f48f2367386d44a2773
wagnercastillo <wagner.castillo@unl.edu.ec>
2023-03-11 17:31:28 -0500

    Add of Methods: Create, Get, GetByID and Delete

--- 03-teslo-shop/src/products/entities/product.entity.ts
+++ 03-teslo-shop/src/products/entities/product.entity.ts
@@ -7,6 +7,7 @@ export class Product {
     @PrimaryGeneratedColumn('uuid')
     id: string; 
 
+    
     @Column('text', { 
         unique: true,
     })
--- 03-teslo-shop/src/products/products.service.ts
+++ 03-teslo-shop/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -30,24 +30,40 @@ export class ProductsService {
     } catch (error) {
       this.handleDBExeptions(error)
     }
-
-    return 'This action adds a new product';
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+
+      return this.productRepository.find({});
+  
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+
+      const product = await this.productRepository.findOne({
+        where: { id: id }
+      })
+            
+      if( !product )
+        throw new NotFoundException(`Product with id '${id}' not found'`)
+      
+      return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+
+    try {
+      
+      await this.productRepository.delete(id)
+
+    } catch (error) {
+       throw new error(error)
+    }
+  
   }
 
 
